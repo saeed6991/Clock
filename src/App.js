1b584210347a9fddd820b947b35ecc0988e5f7bc
unknown <saeedkhalife@gmail.com>
2023-04-29 16:04:08 +0200

    2

--- src/App.js
+++ src/App.js
@@ -3,134 +3,264 @@ import { useEffect, useState } from "react";
 function App() {
   const [blDisplay, setBLDisplay] = useState("5");
   const [slDisplay, setSLDisplay] = useState("25");
-  const [clockRunning, setClockRunning] = useState("0");
   const [time, setTime] = useState(new Date());
   const [clockSeconds, setClockSeconds] = useState("0");
   const [clockMinute, setClockMinutes] = useState("");
+  const [timerState, setTimerState] = useState("0");
+  const [prevState, setPrevState] = useState("1");
+  const [title, setTitle] = useState("Session");
+  /* timerState=0 is the initial state, timerState=1 is when the button play is pushed*/
   const blIncrement = () => {
+    if (timerState === "6") {
+      setTimerState("7");
+      setClockSeconds("0");
+    }
     if (parseInt(blDisplay) < 60) {
       setBLDisplay(parseInt(blDisplay) + 1);
     } else {
-
     }
-  }
+  };
   const blDecrement = () => {
+    if (timerState === "6") {
+      setTimerState("7");
+      setClockSeconds("0");
+    }
     if (parseInt(blDisplay) > 1) {
       setBLDisplay(parseInt(blDisplay) - 1);
     } else {
-
     }
-  }
+  };
   const slIncrement = () => {
+    console.log(timerState);
+    if (timerState === "2") {
+      setTimerState("3");
+      setClockSeconds("0");
+    }
+    
     if (parseInt(slDisplay) < 60) {
       setSLDisplay(parseInt(slDisplay) + 1);
     } else {
-      
-    }  
-  }
+    }
+  };
   const slDecrement = () => {
+    if (timerState === "2") {
+      setTimerState("3");
+    }
+    
     if (parseInt(slDisplay) > 1) {
-          setSLDisplay(parseInt(slDisplay) - 1);
-        } else {
-          
-        }  
-  }
+      setSLDisplay(parseInt(slDisplay) - 1);
+    } else {
+    }
+  };
   const resetPushed = () => {
     setBLDisplay("5");
     setSLDisplay("25");
-    setClockRunning("0");
     setClockMinutes("25");
-  }
+    setClockSeconds("0");
+    setTimerState("0");
+  };
   const changeRunning = () => {
-    if (parseInt(clockRunning) == 0) {
-      setClockRunning("1");
-    } else {
-      setClockRunning("0");
+    if (timerState === "1") {
+      setTimerState("2");
+      console.log(timerState)
+    } else if (timerState === "2") {
+      console.log(timerState)
+      setTimerState("1");
+    } else if (timerState === "0") {
+      setTimerState("1");
+    } else if (timerState === "3") {
+      setTimerState("1");
+    } else if (timerState === "5") {
+      setTimerState("6");
+    } else if (timerState === "7") {
+      setTimerState("5");
+    } else if (timerState === "6") {
+      setTimerState("5");
     }
-  }
-
+  };
   const hours = time.getHours();
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-      if (parseInt(clockRunning) === 1) {
-        if (parseInt(clockSeconds) === 0) {
-          if (parseInt(clockMinute) === 0) {
-            setClockRunning("0");
-            setClockMinutes(slDisplay);
-          } else {
-            setClockMinutes(parseInt(clockMinute) - 1);
-            setClockSeconds(59);
-          }
+    if (timerState == "0") {
+      setClockMinutes(slDisplay);
+      console.log(timerState);
+    }
+  }, [slDisplay, clockMinute, clockSeconds, timerState]);
+
+  useEffect(() => {
+    if (timerState === "1") {
+      const interval = setInterval(() => {
+        if ((clockMinute == "0") && (clockSeconds == "0")) {
+          //beep
+          setTimeout(()=>{
+            setTimerState("4");
+            document.getElementById("beep").play();
+          }, 1000);
+          setClockMinutes(blDisplay);
+        }
+         else if ((clockMinute != "0") && (clockSeconds == "0")) {
+          setClockSeconds("59");
+          setClockMinutes(parseInt(clockMinute) - 1);
         } else {
           setClockSeconds(parseInt(clockSeconds) - 1);
         }
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [clockRunning, clockSeconds, clockMinute, slDisplay]);
+        setTime(new Date);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [clockMinute, clockSeconds, timerState,]);
 
   useEffect(() => {
-    if ( clockRunning == "0") {
+    if (timerState === "3") {
       setClockMinutes(slDisplay);
+      console.log(timerState);
     }
-  }, [slDisplay]);
+  }, [clockMinute, clockSeconds, timerState, slDisplay]);
 
   useEffect(() => {
-    if (parseInt(clockRunning) === 0) {
+    if (timerState == "4") {
+      setClockMinutes(blDisplay);
       setClockSeconds("0");
-      setClockMinutes(slDisplay);
-    } else {
-      setClockSeconds("0");
-      setClockMinutes(slDisplay);
+      console.log(timerState);
+      //document.getElementById("beep").play();
+      setTimerState("5");
+      setPrevState("5");
     }
-  }, [blDisplay, slDisplay, clockRunning]);
- 
-  const doNothing = ()=> {return ""}
+  }, [blDisplay, clockMinute, clockSeconds, timerState, prevState]);
+
+  useEffect(() => {
+    if (timerState === "5") {
+      setTitle("Break");
+      const interval = setInterval(() => {
+        if ((clockMinute == "0") && (clockSeconds == "0")) {
+          //
+          setTimeout(() => {
+            setClockMinutes(slDisplay);
+            setClockSeconds("0");
+            setTimerState("1");
+            document.getElementById("beep").play();
+          }, 1000);
+        }
+         else if ((clockMinute != "0") && (clockSeconds == "0")) {
+          setClockSeconds("59");
+          setClockMinutes(parseInt(clockMinute) - 1);
+        } else {
+          setClockSeconds(parseInt(clockSeconds) - 1);
+        }
+        setTime(new Date);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [clockMinute, clockSeconds, timerState,slDisplay,]);
+
+  useEffect(() => {
+    if (timerState=="6") {
+      
+    }
+  }, [timerState]);
+
+  useEffect(() => {
+    if (timerState === "7") {
+      setClockMinutes(blDisplay);
+      console.log(timerState);
+    }
+  }, [clockMinute, clockSeconds, timerState, blDisplay]);
+
+  useEffect(() => {
+    if ((timerState=="5")||(timerState=="6")||(timerState=="4")) {
+      setTitle("Break");
+    } else if ((timerState=="1")||(timerState=="2")||(timerState=="3")) {
+      console.log(timerState)
+      setTitle("Session");
+    }
+  }, [title, timerState]);
+
+  const doNothing = () => {
+    return "";
+  };
   return (
     <div className="App">
       <div className="title">25 + 5 Clock</div>
-
-      <div className="lengthTexts"> 
+      <div className="lengthTexts">
         <div className="breakLengthContainer">
           <h2 className="breakLengthText">Break length</h2>
           <div className="breakLengthTimerContainer">
-            <div class="arrowUp" onClick={clockRunning=="0"?blIncrement:doNothing}>
+            <div
+              class="arrowUp"
+              onClick={
+                timerState === "0" || timerState === "2" || timerState === "3" || timerState === "6" || timerState === "7"
+                  ? blIncrement
+                  : doNothing
+              }
+            >
               <div className="upperTriangle"></div>
               <div className="arrowStem"></div>
             </div>
-            <div className="BLTimer"><h3>{blDisplay}</h3></div>
-            <div class="arrowDown" onClick={clockRunning=="0"?blDecrement:doNothing}>
+            <div className="BLTimer">
+              <h3>{blDisplay}</h3>
+            </div>
+            <div
+              class="arrowDown"
+              onClick={
+                timerState === "0" || timerState === "2" || timerState === "3" || timerState === "6" || timerState === "7"
+                  ? blDecrement
+                  : doNothing
+              }
+            >
               <div className="arrowStem"></div>
               <div className="lowerTriangle"></div>
             </div>
           </div>
         </div>
-        
+
         <div className="sessionLengthContainer">
           <h2 className="sessionLengthText">Session length</h2>
           <div className="sessionLengthTimeContainer">
-            <div class="arrowUp" onClick={clockRunning=="0"?slIncrement:doNothing}>
+            <div
+              class="arrowUp"
+              onClick={
+                timerState === "0" || timerState === "2" || timerState === "3" || timerState === "6" || timerState === "7"
+                  ? slIncrement
+                  : doNothing
+              }
+            >
               <div className="upperTriangle"></div>
               <div className="arrowStem"></div>
             </div>
-            <div className="SLTimer"><h3>{slDisplay}</h3></div>
-            <div class="arrowDown" onClick={clockRunning=="0"?slDecrement:doNothing}>
+            <div className="SLTimer">
+              <h3>{slDisplay}</h3>
+            </div>
+            <div
+              class="arrowDown"
+              onClick={
+                timerState === "0" || timerState === "2" || timerState === "3" || timerState === "6" || timerState === "7"
+                  ? slDecrement
+                  : doNothing
+              }
+            >
               <div className="arrowStem"></div>
               <div className="lowerTriangle"></div>
             </div>
           </div>
         </div>
       </div>
-      
+
       <div className="session">
-        <h2>Session</h2>
+        <h2>{title}</h2>
         <div className="display">
-          <h1>{clockMinute}:{clockSeconds < 10?`0${clockSeconds}`:clockSeconds}</h1>
-          <div className="time"><h1>{hours < 10 ? `0${hours}` : hours}:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1></div>
+          <h1>
+            {clockMinute}:
+            {clockSeconds < 10 ? `0${clockSeconds}` : clockSeconds}
+          </h1>
+          <div className="time">
+            <h1>
+              {hours < 10 ? `0${hours}` : hours}:
+              {minutes < 10 ? `0${minutes}` : minutes}:
+              {seconds < 10 ? `0${seconds}` : seconds}
+            </h1>
+          </div>
         </div>
         <div className="buttons">
           <div className="triangle" onClick={changeRunning}></div>
@@ -141,10 +271,8 @@ function App() {
           <div className="square" id="reset" onClick={resetPushed}>
             <p className="reset">Reset</p>
           </div>
-        </div>      
+        </div>
       </div>
-
-      
     </div>
   );
 }
